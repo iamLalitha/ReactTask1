8e8ec263c830c9dd52320c9081ce993f8658909a
iamLalitha <lalithasans10@gmail.com>
2023-06-23 18:53:00 +0530

    day10

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-//
-import React, {useState, useRef} from 'react'
+// //
+// import React, {useState, useRef} from 'react'
 
-function App() {
-    const inputRef=useRef(null);
-    const[text,setText]=useState('no text');
-  let  handleButtonClick=()=>{
-    //get the value from the input box
-    let value=useRef.current.value;
-    setText(value);
-    console.log(inputRef);
-  }
+// function App() {
+//     const inputRef=useRef(null);
+//     const[text,setText]=useState('no text');
+//   let  handleButtonClick=()=>{
+//     //get the value from the input box
+//     let value=useRef.current.value;
+//     setText(value);
+//     console.log(inputRef);
+//   }
+//   return (
+//     <div>
+//         <input type="text"/>
+//         <button onClick={handleButtonClick}>Get Text</button>
+//         <p>Entered Text:{text}</p>
+//     </div>
+//   )
+// }
+
+// export default App
+//DAY 10
+//A simple note taking application
+import React from 'react'
+
+function App(props) {
   return (
     <div>
-        <input type="text"/>
-        <button onClick={handleButtonClick}>Get Text</button>
-        <p>Entered Text:{text}</p>
+      <h1>Notes</h1>
+      <ul>
+        
+      </ul>
     </div>
   )
 }
