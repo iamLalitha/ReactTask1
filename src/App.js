e92304521787f2ee1b061298e2a93fa3860aaf60
iamLalitha <lalithasans10@gmail.com>
2023-06-23 21:38:02 +0530

    day10push

--- src/App.js
+++ src/App.js
@@ -22,17 +22,73 @@
 // export default App
 //DAY 10
 //A simple note taking application
-import React from 'react'
+import React, { useState } from 'react'
 
+
+function Note({note}){
+  return(
+    <li>{note.content}</li>
+  )
+}
+ 
 function App(props) {
+
+ //define a state
+ const [notes,setNotes]=useState(props.notes);
+ const [newNote,setNewNote]=useState('');
+ const [newNoteImportant, setNewNoteImportant]=useState('');
+
+ //get te data
+ useEffect(()=>{
+  setNotes(props.notes);
+ })
+
+ //create a reference for the first input text box
+ const newNoteContentRef=useRef(null);
+
+//define addnote function
+let addNote=(event)=>{
+ event.preventDefault();
+ //create a new object
+ let noteObject={
+  id:notes.length+1,
+  content:newNoteContent,
+  important: newNoteImportant,
+}
+// add the new object to the notes state
+// console.log(noteObject);
+setNotes(notes.concat(noteObject));
+
+//clear the input text box
+setNewNote('...new note');
+}
+
+
+
+let handleNotChange=(event)=>{
+ setNewNote(event.target.value);
+
+}
   return (
     <div>
       <h1>Notes</h1>
       <ul>
-        
+        {notes.map(note=>
+        <Note key={note.id} note={note}/>
+        )}
       </ul>
+      {/* add a simple form for adding notes */}
+<form onSubmit={addNote}>
+  <input value={newNoteContent} 
+  onChange={handleNotChange}
+  placeholder='type a note'
+  ref={newNoteContentRef}/>
+  <br/>
+  <input type='text' placeholder='enter true or false' value={newNoteImportant} onChange={(e)=>setNewNoteImportant(e.target.value)}/>
+  <button type='submit'>Add Note</button>
+</form>
     </div>
   )
 }
 
-export default App
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,6 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-
-
 
 
 // const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -14,4 +12,35 @@ import App from './App';
 //strict mode is used to identify any deprecated tags are there or not. it just to check.
 //the below line removes the strict mode. it is same as like above code lines from 6-10. 
 //the below code jus removes the stirct mode.
-ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
+
+//day3
+const notes=[
+    {
+        id:1,
+        content:'Working with Lists',
+        important:true
+    },
+    {
+        id:2,
+        content:'Usage of keys',
+        important:false
+    },
+    {
+        id:3,
+        content:'Conditional Rendering',
+        important:true
+    },
+    {
+        id:4,
+        content:'Handling Events',
+        important:true
+    },
+    {
+        id:5,
+        content:'Props vs State',
+        important:false
+    },
+]
+//passing the above notes data to the app i.e passing the notes as 'props'
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes}/>);
+
