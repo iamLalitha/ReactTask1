83c73db46fb7feb8dd6d00edef3cc0f89f6e3b67
iamLalitha <lalithasans10@gmail.com>
2023-06-25 23:41:37 +0530

    day10cssadded

--- src/App.js
+++ src/App.js
@@ -30,88 +30,100 @@ import CreateNote from './components/createNote';
 import ReadNotes from './components/ReadNotes';
 import Home from './components/Home';
 import EditNote from './components/EditNote';
-import {Link, Route, BrowserRouter as Router, Routes} from 'react-router-dom';
+import { Link, Route, BrowserRouter as Router, Routes } from 'react-router-dom';
 
 
 function App() {
-  
-  // define a state
   const [notes, setNotes] = useState([]);
   const [newNoteContent, setNewNoteContent] = useState('');
   const [newNoteImportant, setNewNoteImportant] = useState('');
   const [showStatus, setShowStatus] = useState('all');
- const padding ={
-  padding:15
- };
- 
-  // get the data
-  
-  useEffect(() => {
-    // setNotes(props.notes);
-    //making axios call
-    axios
-    .get('http://localhost:3001/notes')
-    .then(response=> setNotes(response.data));
-  });
-
-  // create a reference for the first input text box
   const newNoteContentRef = useRef(null);
- // define the addNote method
+
+  useEffect(() => {
+    axios.get('http://localhost:3001/notes').then((response) => setNotes(response.data));
+  }, []);
+
   let addNote = (event) => {
     event.preventDefault();
-     // create a new object
     let noteObject = {
       id: notes.length + 1,
       content: newNoteContent,
-      important: newNoteImportant==='true',
-    }
-    // add the new object to the notes state
-    // setNotes(notes.concat(noteObject));
+      important: newNoteImportant === 'true',
+    };
 
-    //send the record to the json server 
-    axios
-    .post('http://localhost:3001/notes', noteObject)
-    // .then(response=> console.log(response));
-    // console.log(noteObject);
+    axios.post('http://localhost:3001/notes', noteObject);
 
-    // clear the input text box
     setNewNoteContent('');
     setNewNoteImportant('');
     newNoteContentRef.current.focus();
-  }
+  };
 
   let handleNoteChange = (event) => {
     setNewNoteContent(event.target.value);
-  }
+  };
 
   let handleSelectChange = (event) => {
     setNewNoteImportant(event.target.value);
-  }
+  };
 
   let handleStatusChange = (event) => {
     setShowStatus(event.target.value);
-    // console.log(event.target.value);
-  }
-  
+  };
+
   return (
     <Router>
+      <div className="container">
+        <nav className="navigation">
+          <Link className="navigation-link" to="/">
+            Home
+          </Link>
+          <Link className="navigation-link" to="/read">
+            Read Notes
+          </Link>
+          <Link className="navigation-link" to="/create">
+            Create Notes
+          </Link>
+          <Link className="navigation-link" to="/edit">
+            Edit Notes
+          </Link>
+        </nav>
+      </div>
 
-    <div>
-      <Link style={padding} to="/">Home</Link>
-      <Link style={padding} to="/read">Read Notes</Link>
-      <Link style={padding} to="/create">Create Notes</Link>
-      <Link style={padding} to="/edit">Edit Notes</Link>
-    </div>
-
-    <Routes>
-      <Route path='/read' element={<ReadNotes showStatus={showStatus} handleStatusChange={handleStatusChange} notes={notes} />} />
-      <Route path='/create' element={<CreateNote addNote={addNote} newNoteContent={newNoteContent} handleNoteChange={handleNoteChange} newNoteContentRef={newNoteContentRef} handleSelectChange={handleSelectChange} newNoteImportant={newNoteImportant} />} />
-      <Route path='/' element={<Home />} />
-      <Route path='/edit' element={<EditNote />} />
-    </Routes>
-  </Router>
-
-  )
+      <div className="content">
+        <Routes>
+          <Route
+            path="/read"
+            element={
+              <ReadNotes
+                id="read-notes"
+                showStatus={showStatus}
+                handleStatusChange={handleStatusChange}
+                notes={notes}
+              />
+            }
+          />
+          <Route
+            path="/create"
+            element={
+              <CreateNote
+                id="create-note"
+                addNote={addNote}
+                newNoteContent={newNoteContent}
+                handleNoteChange={handleNoteChange}
+                newNoteContentRef={newNoteContentRef}
+                handleSelectChange={handleSelectChange}
+                newNoteImportant={newNoteImportant}
+              />
+            }
+          />
+          <Route path="/" element={<Home />} />
+          <Route path="/edit" element={<EditNote />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
+
